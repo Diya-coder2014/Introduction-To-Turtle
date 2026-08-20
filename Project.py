db381bac9e22824bcfdc709e448d314915e80c0f
Diya-coder2014 <stu13311@dpsmisdoha.com>
2026-08-20 17:36:57 +0530

    All Activities and Questions complete.

---
+++ Project.py
@@ -0,0 +1,16 @@
+import turtle
+turtle.Screen().bgcolor("cyan")
+sq = turtle.Turtle()
+
+sq.forward(190)
+
+sq.right(90)
+sq.forward(190)
+
+sq.right(90)
+sq.forward(190)
+
+sq.right(90)
+sq.forward(190)
+
+turtle.done()
